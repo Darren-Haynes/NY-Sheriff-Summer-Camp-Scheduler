23375eaf4abe06f46f97aa3282e4564e4034b715
Darren Haynes <darren.haynes.mr@outlook.com>
2025-11-02 23:43:44 -0600

    header-logo
    Add logo to header and install and config the use of png files. 'npm install file-loader was needed for webpack to accept png files.

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import Header from "./Header";
+import MainLogo from "./MainLogo";
 
 const root = createRoot(document.body);
 root.render(<App />);
@@ -8,6 +9,7 @@ root.render(<App />);
 function App() {
   return (
     <div>
+      <MainLogo />
       <Header />
     </div>
   );
---
+++ src/renderer/MainLogo.tsx
@@ -0,0 +1,9 @@
+import Logo from "../../assets/sheriffs-logo.png";
+
+export default function MainLogo() {
+  return (
+    <div id="logo">
+      <img width="199" alt="icon" src={Logo} />
+    </div>
+  );
+}
