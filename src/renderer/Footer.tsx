bb9ea1adeddcabf95c1922d7e355c5fa7f3b59e5
Darren Haynes <darren.haynes.mr@outlook.com>
2025-11-02 23:49:23 -0600

    Footer

---
+++ src/renderer/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        © Copyright 2025. New York State Sheriffs&apos; Institute. All rights
+        reserved.
+      </p>
+    </footer>
+  );
+}
