2506ebaa373d39bfd6eeb47ecf13678fb9721cf5
Darren Haynes <darren.haynes.mr@outlook.com>
2025-11-02 22:56:54 -0600

    Navbar

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import Header from "./Header";
 
 const root = createRoot(document.body);
-root.render(<h2>Hello from React!</h2>);
+root.render(<App />);
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
