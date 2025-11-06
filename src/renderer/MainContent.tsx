a8543e3f80945104b5324dd62834235262c2d330
Darren Haynes <darren.haynes.mr@outlook.com>
2025-11-05 23:41:08 -0600

    Toggle components
    Use react state instead of excessive typescript code in 'paste-box.ts' to toggle the component visibility. Much cleaner.

--- src/renderer/MainContent.tsx
+++ src/renderer/MainContent.tsx
@@ -1,43 +1,79 @@
+import React, { useState } from "react";
 import CampSign from "../../assets/its-all-about-the-kids.png";
-import { openTxtBox, closeTxtBox } from "./paste-box";
+
+interface ToggleProps {
+  isVisible: boolean;
+  onToggle: React.MouseEventHandler<HTMLButtonElement>;
+}
 
 export default function MainContent() {
+  const [showInputOptions, setShowInputOptions] = useState(true);
+
+  const handleToggle = () => {
+    setShowInputOptions((prev) => !prev);
+  };
+
   return (
     <main>
       <div className="bg-image">
         <div className="overlay">
           <div id="input-section">
-            <div id="input-box">
-              <p>Upload a spreadsheet</p>
-
-              <button type="button" id="upload-btn" className="btn-margin">
-                Upload
-              </button>
-              <p>or paste sheet content</p>
-              <button
-                onClick={openTxtBox}
-                type="button"
-                id="paste-btn"
-                className="paste-btn-txt btn-margin"
-              >
-                Paste Sheet
-              </button>
-              <button
-                onClick={closeTxtBox}
-                type="button"
-                id="close-btn"
-                className="btn-margin"
-              >
-                Close
-              </button>
-
-              <div id="camp-sign">
-                <img width="400" alt="icon" src={CampSign} />
-              </div>
-            </div>
+            <InputOptions
+              isVisible={showInputOptions}
+              onToggle={handleToggle}
+            />
+            <PasteBox isVisible={!showInputOptions} onToggle={handleToggle} />
           </div>
         </div>
       </div>
     </main>
   );
 }
+
+const InputOptions: React.FC<ToggleProps> = ({ isVisible, onToggle }) => {
+  if (!isVisible) {
+    return null;
+  }
+  return (
+    <div id="input-options">
+      <p>Upload a spreadsheet</p>
+
+      <button type="button" id="upload-btn" className="btn-margin">
+        Upload
+      </button>
+      <p>or paste sheet content</p>
+      <button
+        onClick={onToggle}
+        type="button"
+        id="paste-btn"
+        className="paste-btn-txt btn-margin"
+      >
+        Paste Sheet
+      </button>
+
+      <div id="camp-sign">
+        <img width="400" alt="icon" src={CampSign} />
+      </div>
+    </div>
+  );
+};
+
+const PasteBox: React.FC<ToggleProps> = ({ isVisible, onToggle }) => {
+  if (!isVisible) {
+    console.log(typeof isVisible);
+    console.log(typeof onToggle);
+    return null;
+  }
+  return (
+    <div id="texty-boxy">
+      <button
+        onClick={onToggle}
+        type="button"
+        id="close-btn"
+        className="btn-margin"
+      >
+        Close
+      </button>
+    </div>
+  );
+};
