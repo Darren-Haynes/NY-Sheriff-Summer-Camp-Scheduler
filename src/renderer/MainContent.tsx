d2959d7791afe86a18c472d693228b7be538fbec
Darren Haynes <darren.haynes.mr@outlook.com>
2025-11-05 23:51:59 -0600

    Move components to their own files.

--- src/renderer/MainContent.tsx
+++ src/renderer/MainContent.tsx
@@ -1,10 +1,6 @@
 import React, { useState } from "react";
-import CampSign from "../../assets/its-all-about-the-kids.png";
-
-interface ToggleProps {
-  isVisible: boolean;
-  onToggle: React.MouseEventHandler<HTMLButtonElement>;
-}
+import InputOptions from "./InputOptions";
+import PasteBox from "./PasteBox";
 
 export default function MainContent() {
   const [showInputOptions, setShowInputOptions] = useState(true);
@@ -29,49 +25,3 @@ export default function MainContent() {
     </main>
   );
 }
-
-const InputOptions: React.FC<ToggleProps> = ({ isVisible, onToggle }) => {
-  if (!isVisible) {
-    return null;
-  }
-  return (
-    <div id="input-options">
-      <p>Upload a spreadsheet</p>
-
-      <button type="button" id="upload-btn" className="btn-margin">
-        Upload
-      </button>
-      <p>or paste sheet content</p>
-      <button
-        onClick={onToggle}
-        type="button"
-        id="paste-btn"
-        className="paste-btn-txt btn-margin"
-      >
-        Paste Sheet
-      </button>
-
-      <div id="camp-sign">
-        <img width="400" alt="icon" src={CampSign} />
-      </div>
-    </div>
-  );
-};
-
-const PasteBox: React.FC<ToggleProps> = ({ isVisible, onToggle }) => {
-  if (!isVisible) {
-    return null;
-  }
-  return (
-    <div id="texty-boxy">
-      <button
-        onClick={onToggle}
-        type="button"
-        id="close-btn"
-        className="btn-margin"
-      >
-        Close
-      </button>
-    </div>
-  );
-};
