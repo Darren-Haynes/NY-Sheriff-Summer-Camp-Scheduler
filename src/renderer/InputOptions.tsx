ca899678ad8d4220e81bd578371d0232ac803d66
Darren Haynes <darren.haynes.mr@outlook.com>
2025-11-06 00:58:42 -0600

    Responsive input-options box complete.

--- src/renderer/InputOptions.tsx
+++ src/renderer/InputOptions.tsx
@@ -1,3 +1,4 @@
+import { useMediaQuery } from "react-responsive";
 import CampSign from "../../assets/its-all-about-the-kids.png";
 
 interface ToggleProps {
@@ -9,8 +10,21 @@ const InputOptions: React.FC<ToggleProps> = ({ isVisible, onToggle }) => {
   if (!isVisible) {
     return null;
   }
+
+  const showCampSign = useMediaQuery({ query: "(min-height: 825px)" });
+  const showOnLargeScreen = useMediaQuery({ query: "(min-height: 1200px)" });
+
+  const campSignStyles = {
+    height: showCampSign ? "500px" : "300px",
+    marginTop: showOnLargeScreen
+      ? "calc((100vh - 1100px) / 2)"
+      : showCampSign
+        ? "calc((100vh - 790px) / 2)"
+        : "calc(((100vh - 267px) - 310px) / 2)",
+  };
+
   return (
-    <div id="input-options">
+    <div id="input-options" style={campSignStyles}>
       <p>Upload a spreadsheet</p>
 
       <button type="button" id="upload-btn" className="btn-margin">
@@ -26,9 +40,11 @@ const InputOptions: React.FC<ToggleProps> = ({ isVisible, onToggle }) => {
         Paste Sheet
       </button>
 
-      <div id="camp-sign">
-        <img width="400" alt="icon" src={CampSign} />
-      </div>
+      {showCampSign && (
+        <div id="camp-sign">
+          <img width="400" alt="icon" src={CampSign} />
+        </div>
+      )}
     </div>
   );
 };
