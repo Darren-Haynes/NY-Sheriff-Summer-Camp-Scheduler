a5d7dd59e00c9a3076485a19f509a1d862bb2570
Darren Haynes <darren.haynes.mr@outlook.com>
2025-11-09 20:16:50 -0600

    File upload dialog for InputOptions box.

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,7 +1,8 @@
 import { contextBridge, ipcRenderer } from "electron"
 
 const preload = {
-    send_text: (message: string) => ipcRenderer.invoke('submit-text', message)
+    send_text: (message: string) => ipcRenderer.invoke('submit-text', message),
+    file_dialog: () => ipcRenderer.invoke('open-file-dialog')
 }
 
 contextBridge.exposeInMainWorld('textAPI', preload)
