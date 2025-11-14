60ed0f93da1c14cea9ee7deeeae40266df871d9e
Darren Haynes <darren.haynes.mr@outlook.com>
2025-11-13 22:33:45 -0600

    Responsive header text.
    Fix header text so that it looks more equally spaced and responsive on each side of the header logo.

--- src/renderer/Header.tsx
+++ src/renderer/Header.tsx
@@ -1,12 +1,32 @@
+import { useMediaQuery } from "react-responsive";
+
 export default function Header() {
+  {
+    /* Responsive "Summer Camp Scheduler" header title */
+  }
+  const headerAlign700 = useMediaQuery({ query: "(max-width: 700px)" });
+  const headerAlign900 = useMediaQuery({ query: "(max-width: 900px)" });
+  const headerLeft = {
+    textAlign: "right" as const,
+    marginRight: headerAlign700 ? 109 : headerAlign900 ? 120 : 150,
+  };
+  const headerRight = {
+    textAlign: "left" as const,
+    marginLeft: headerAlign900 ? 120 : 150,
+  };
+
   return (
     <header>
       <div id="navbar">
         <div className="navbar-content">
-          <h1 id="navbar-left">Summer Camp</h1>
+          <h1 id="navbar-left" style={headerLeft}>
+            Summer Camp
+          </h1>
         </div>
         <div className="navbar-content">
-          <h1 id="navbar-right">Scheduler</h1>
+          <h1 id="navbar-right" style={headerRight}>
+            Scheduler
+          </h1>
         </div>
       </div>
     </header>
