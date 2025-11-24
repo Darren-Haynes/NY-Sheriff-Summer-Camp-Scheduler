6be42f726e37d7052d01a10c10614c534ada29e4
Darren Haynes <darren.haynes.mr@outlook.com>
2025-11-24 00:23:48 -0600

    Receive user Data errors from main.

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,8 +1,10 @@
 import { contextBridge, ipcRenderer } from 'electron';
 
 const preload = {
-    send_text: (message: string) => ipcRenderer.invoke('submit-text', message),
-    file_dialog: () => ipcRenderer.invoke('open-file-dialog'),
+  send_text: (message: string) => ipcRenderer.invoke('submit-text', message),
+  file_dialog: () => ipcRenderer.invoke('open-file-dialog'),
+  // send_error: (errorData: []) => ipcRenderer.on('error-list', errorData),
+  send_error: callback => ipcRenderer.on('error-list', (_event, value) => callback(value)),
 };
 
 contextBridge.exposeInMainWorld('textAPI', preload);
