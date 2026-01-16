0df30960a120678351ba894984e9fbdef52e1618
Darren Haynes <darren.haynes.mr@outlook.com>
2026-01-16 03:04:42 -0600

    Create kids Map object from user data.

--- src/main/scheduler.ts
+++ src/main/scheduler.ts
@@ -1,13 +1,51 @@
-class Scheduler {
-    mainData: string;
-    constructor() {
-        this.mainData = this.parseData();
+/**
+Wrapper for map that stores all kids attending and their prefered activites.
+*/
+class Kid {
+  kidsMap: Map<
+    string,
+    {
+      land1: string;
+      land2: string;
+      land3: string;
+      water1: string;
+      water2: string;
+      water3: string;
     }
-
-    parseData(): string {
-        console.log('test log');
-        return 'test';
-    }
+  >;
+  constructor() {
+    this.kidsMap = new Map();
+  }
 }
 
-const scheduler = new Scheduler();
+/**
+Main class that schedules the kids to activities.
+*/
+export class Scheduler {
+  inputData: string;
+  inputDataArr: Array<string>;
+  kids: Kid;
+  col: Array<string>;
+
+  constructor(inputData: string) {
+    this.inputData = inputData;
+    this.kids = new Kid();
+  }
+
+  public parseData(): void {
+    const inputDataArr = this.inputData.split('\n');
+    inputDataArr.pop(); // remove last empty line
+    inputDataArr.forEach((line: string) => {
+      const col = line.split('\t');
+      const name = col[0] + ' ' + col[1];
+      this.kids.kidsMap.set(name, {
+        land1: col[3],
+        land2: col[4],
+        land3: col[5],
+        water1: col[6],
+        water2: col[7],
+        water3: col[8],
+      });
+    });
+  }
+}
