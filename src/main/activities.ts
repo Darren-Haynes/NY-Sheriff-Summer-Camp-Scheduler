ce39e8393bc611201a9dbb188755a49aefcdc096
Darren Haynes <darren.haynes.mr@outlook.com>
2026-02-14 18:25:49 -0600

    change activity order by first min required kids and then lexicographical.
    Why? To prepare for upcoming methods that would need to sort activities to be 'require min and then lexicographical'. This should avoid some sorting and create clearer code, lets see if the future agrees.

--- src/main/activities.ts
+++ src/main/activities.ts
@@ -3,44 +3,43 @@ import { CampActivities } from '../types/camp-types';
 export const Activities: CampActivities = Object.freeze({
   // Shortened names of all the land activities offered from both 9am and 10am.
   landActs: [
-    'bball',
-    'vball',
-    'soc',
-    'arch',
+    'fris',
     'art',
     'hike',
-    'cheer',
     'pball',
-    'lax',
+    'bball',
+    'cheer',
     'fball',
+    'lax',
+    'soc',
+    'vball',
     'yoga',
-    'fris',
+    'arch',
   ],
 
   // Shortened names of all the land activities offered at 9am.
-  land9amActs: ['bball', 'vball', 'soc', 'arch', 'art', 'hike', 'cheer'],
+  land9amActs: ['art', 'hike', 'bball', 'cheer', 'soc', 'vball', 'arch'],
   // Shortened names of all the land activities offered at 10am.
-  land10amActs: ['pball', 'lax', 'fball', 'yoga', 'fris', 'arch', 'art', 'hike'],
+  land10amActs: ['fris', 'art', 'hike', 'pball', 'fball', 'lax', 'yoga', 'arch'],
   // Shortened names of all the water activities offered.
-  waterActs: ['swim', 'fish', 'canoe', 'snork', 'sail', 'pboard', 'kayak'],
+  waterActs: ['fish', 'pboard', 'snork', 'canoe', 'kayak', 'sail', 'swim'],
 
   // To store all the kids scheduled for water activities at 9am.
-  water9am: { swim: [], fish: [], canoe: [], snork: [], sail: [], pboard: [], kayak: [] },
+  water9am: { fish: [], pboard: [], snork: [], canoe: [], kayak: [], sail: [], swim: [] },
   // To store all the kids scheduled for water activities at 10am.
-  water10am: { swim: [], fish: [], canoe: [], snork: [], sail: [], pboard: [], kayak: [] },
+  water10am: { fish: [], pboard: [], snork: [], canoe: [], kayak: [], sail: [], swim: [] },
   // To store all the kids scheduled for land activities at 9am.
-  land9am: { bball: [], vball: [], soc: [], arch: [], art: [], hike: [], cheer: [] },
+  land9am: { art: [], hike: [], bball: [], cheer: [], soc: [], vball: [], arch: [] },
   // To store all the kids scheduled for land activities at 10am.
   land10am: {
-    pball: [],
-    lax: [],
-    fball: [],
-    yoga: [],
     fris: [],
-    arch: [],
     art: [],
     hike: [],
-    cheer: [],
+    pball: [],
+    fball: [],
+    lax: [],
+    yoga: [],
+    arch: [],
   },
 
   /*
@@ -50,18 +49,18 @@ export const Activities: CampActivities = Object.freeze({
    // array[2] 0 = 9am only, 1 = 10am only, 2 = 9am & 10am activities
 */
   landRanges: {
-    bball: [6, 12, 0],
-    vball: [6, 12, 0],
-    soc: [6, 12, 0],
-    cheer: [6, 12, 0],
-    pball: [4, 12, 1],
-    lax: [6, 12, 1],
-    fball: [6, 12, 1],
-    yoga: [6, 10, 1],
     fris: [3, 8, 1],
-    arch: [8, 16, 2],
     art: [4, 10, 2],
     hike: [4, 6, 2],
+    pball: [4, 12, 1],
+    bball: [6, 12, 0],
+    cheer: [6, 12, 0],
+    fball: [6, 12, 1],
+    lax: [6, 12, 1],
+    soc: [6, 12, 0],
+    vball: [6, 12, 0],
+    yoga: [6, 10, 1],
+    arch: [8, 16, 2],
   },
 
   /*
@@ -73,12 +72,12 @@ export const Activities: CampActivities = Object.freeze({
   // array[4] Double the maximum num of people per activity
 */
   waterRanges: {
-    swim: [6, 16, 2, 12, 32],
     fish: [4, 7, 2, 8, 14],
-    canoe: [6, 10, 2, 12, 20],
-    snork: [4, 8, 2, 8, 16],
-    sail: [6, 16, 2, 12, 32],
     pboard: [4, 8, 2, 8, 16],
+    snork: [4, 8, 2, 8, 16],
+    canoe: [6, 10, 2, 12, 20],
     kayak: [6, 8, 2, 12, 16],
+    sail: [6, 16, 2, 12, 32],
+    swim: [6, 16, 2, 12, 32],
   },
 });
