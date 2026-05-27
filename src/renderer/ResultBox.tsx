37841eed2f49c0338c19a59c7b5439448602c6ea
Darren Haynes <darren.haynes.mr@outlook.com>
2026-05-27 00:07:37 -0500

    ResultsBox now prints all activity time slot data

--- src/renderer/ResultBox.tsx
+++ src/renderer/ResultBox.tsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import TimeSlot from './timeSlots';
+
 interface ToggleProps {
   isVisible: string;
   onToggle: React.MouseEventHandler<HTMLButtonElement>;
@@ -9,67 +12,48 @@ interface resultObjects {
   resultList: string[];
 }
 
-const resultBox: React.FC<ToggleProps> = ({ isVisible, onToggle, result }) => {
+const ResultBox: React.FC<ToggleProps> = ({ isVisible, onToggle, result }) => {
   if (isVisible !== 'result-box') {
     return null;
   }
 
-  const activityTimeSlots: string[] = ['water9am', 'water10am'];
   const waterActs: string[] = ['fish', 'pboard', 'snork', 'canoe', 'kayak', 'sail', 'swim'];
   const land9amActs: string[] = ['art', 'hike', 'bball', 'cheer', 'soc', 'vball', 'arch'];
   const land10amActs: string[] = ['fris', 'art', 'hike', 'pball', 'fball', 'lax', 'yoga', 'arch'];
 
-  let waterText = '';
-  activityTimeSlots.forEach(timeSlot => {
-    waterText += `${timeSlot}:\n`;
-    waterActs.forEach(activity => {
-      const names = result[timeSlot][activity];
-      waterText += `${activity}: \n`;
-      if (names.length === 0) {
-        waterText += `No kids scheduled\n\n`;
-      } else {
-        waterText += `${names.join(', ')}\n\n`;
-      }
-    });
-  });
-
-  let landText9am = '';
-  landText9am += `Land 9am:\n`;
-  land9amActs.forEach(activity => {
-    const names = result['land9am'][activity];
-    landText9am += `${activity}: \n`;
-    if (names.length === 0) {
-      landText9am += `No kids scheduled\n\n`;
-    } else {
-      landText9am += `${names.join(', ')}\n\n`;
-    }
-  });
-
-  let landText10am = '';
-  landText10am += `Land 10am:\n`;
-  land10amActs.forEach(activity => {
-    const names = result['land10am'][activity];
-    landText10am += `${activity}: \n`;
-    if (names.length === 0) {
-      landText10am += `No kids scheduled\n\n`;
-    } else {
-      landText10am += `${names.join(', ')}\n\n`;
-    }
-  });
-
-  const allText = waterText + landText9am + landText10am;
   return (
     <div id="result-box">
       <h2 id="result-heading">Camp Schedule:</h2>
-      <h3 className="result-heading-sub">Water 9am</h3>
-      <h3 className="result-heading-sub">Water 10am</h3>
-      <h3 className="result-heading-sub">Land 9am</h3>
-      <h3 className="result-heading-sub">Land 10am</h3>
-      <div id="result-content">
-        <div>{allText}</div>
-      </div>
+      <TimeSlot
+        heading={'Water 9am'}
+        timeSlot={'water9am'}
+        activities={waterActs}
+        result={result}
+      />
+      ;
+      <TimeSlot
+        heading={'Water 10am'}
+        timeSlot={'water10am'}
+        activities={waterActs}
+        result={result}
+      />
+      ;
+      <TimeSlot
+        heading={'Land 9am'}
+        timeSlot={'land9am'}
+        activities={land9amActs}
+        result={result}
+      />
+      ;
+      <TimeSlot
+        heading={'Land 10am'}
+        timeSlot={'land10am'}
+        activities={land10amActs}
+        result={result}
+      />
+      ;
     </div>
   );
 };
 
-export default resultBox;
+export default ResultBox;
