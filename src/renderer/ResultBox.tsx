1440964bcab137b9daa79bd0eae4448574c5f2e0
Darren Haynes <darren.haynes.mr@outlook.com>
2026-05-27 12:38:08 -0500

    Add buttons to Result Box.
    Close, export & copy buttons added to Result Box.
    
    BUG FIX: fixed close button bug in PasteBox.tsx. Incorrect argument was being passed to onToggle() and thus not switching UI back to the InputBox.

--- src/renderer/ResultBox.tsx
+++ src/renderer/ResultBox.tsx
@@ -23,35 +23,66 @@ const ResultBox: React.FC<ToggleProps> = ({ isVisible, onToggle, result }) => {
 
   return (
     <div id="result-box">
-      <h2 id="result-heading">Camp Schedule:</h2>
-      <TimeSlot
-        heading={'Water 9am'}
-        timeSlot={'water9am'}
-        activities={waterActs}
-        result={result}
-      />
-      ;
-      <TimeSlot
-        heading={'Water 10am'}
-        timeSlot={'water10am'}
-        activities={waterActs}
-        result={result}
-      />
-      ;
-      <TimeSlot
-        heading={'Land 9am'}
-        timeSlot={'land9am'}
-        activities={land9amActs}
-        result={result}
-      />
-      ;
-      <TimeSlot
-        heading={'Land 10am'}
-        timeSlot={'land10am'}
-        activities={land10amActs}
-        result={result}
-      />
-      ;
+      <div id="text-box" className="fade-in-1-5s">
+        <div id="result-textarea">
+          <TimeSlot
+            heading={'Water 9am'}
+            timeSlot={'water9am'}
+            activities={waterActs}
+            result={result}
+          />
+          <TimeSlot
+            heading={'Water 10am'}
+            timeSlot={'water10am'}
+            activities={waterActs}
+            result={result}
+          />
+          <TimeSlot
+            heading={'Land 9am'}
+            timeSlot={'land9am'}
+            activities={land9amActs}
+            result={result}
+          />
+          <TimeSlot
+            heading={'Land 10am'}
+            timeSlot={'land10am'}
+            activities={land10amActs}
+            result={result}
+          />
+        </div>
+      </div>
+
+      <div id="result-box-btns-container">
+        <h2 id="result-heading" className="fade-in-1-5s">
+          Camp Schedule
+        </h2>
+        <div id="result-box-btns">
+          <button
+            onClick={() => onToggle('input-box')}
+            type="button"
+            id="close-btn"
+            className="paste-box-btns fade-in-1s"
+          >
+            Close ❌
+          </button>
+          <button
+            // onClick={fileUpload}
+            type="button"
+            id="upload-btn-2"
+            className="paste-box-btns fade-in-3s"
+          >
+            Export 📤
+          </button>
+          <button
+            // onClick={submitTextboxContent}
+            type="button"
+            id="submit-btn"
+            className="paste-box-btns fade-in-5s"
+          >
+            Copy 📋
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
