12f704baad010cfc260e3f8441cbb9e8f296e5a2
Darren Haynes <darren.haynes.mr@outlook.com>
2026-06-16 20:58:54 -0500

    FIX: update to 'appContext' from 'electronWindow'

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from './fixtures'; // 👈 Import from your fixture file
 
-test('Application launches and loads main window', async ({ electronWindow }) => {
+test('Application launches and loads main window', async ({ appContext }) => {
+  const { electronWindow } = appContext;
   // The app is already fully booted up and ready to test here!
   const title = await electronWindow.title();
   console.log(`Loaded app window with title: ${title}`);
