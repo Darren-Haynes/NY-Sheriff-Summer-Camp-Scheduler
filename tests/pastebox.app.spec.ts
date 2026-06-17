266f679f5c93f78691e88acbfde76c4ae0fa9cb0
Darren Haynes <darren.haynes.mr@outlook.com>
2026-06-16 20:58:54 -0500

    FIX: update to 'appContext' from 'electronWindow'

--- tests/pastebox.app.spec.ts
+++ tests/pastebox.app.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from './fixtures';
 
-test('Pastebox accurately processes raw kid schedules', async ({ electronWindow }) => {
+test('Pastebox accurately processes raw kid schedules', async ({ appContext }) => {
+  const { electronWindow } = appContext;
   await electronWindow.locator('button:has-text("Paste")').click();
   const textarea = electronWindow.locator('textarea');
   await expect(textarea).toHaveValue('Paste text here...');
